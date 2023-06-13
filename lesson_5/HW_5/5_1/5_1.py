afa92acb3c83f4317696e1e2ffac15fa2e6c41c3
UBAHKul <ubahkul@gmail.com>
2023-06-13 21:26:19 +0300

    1

---
+++ lesson_5/HW_5/5_1/5_1.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая на вход
+# принимает два числа A и B,
+# и возводит число А в целую.
+# степень B с помощью рекурсии.1.gi
+
+
+
+def pow_num(a, b):
+    if b == 0:
+        return 1
+    if b < 0:
+        return pow_num(a, b + 1) * 1 / a
+    return pow_num(a, b - 1) * a
+
+
+print(pow_num(int(input()), int(input())))
---
+++ lesson_6/HW_6/6_2/6_2.py
@@ -0,0 +1,16 @@
+# Определить индексы элементов массива (списка), значения
+# которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше
+#  заданного максимума)
+
+num_max = int(input())
+num_min = int(input())
+
+import random
+list = [random.randint(1, 100) for _ in range(10)]
+print(list)
+
+
+print([ind for ind, val in enumerate(list) if num_min <= val <= num_max])
+
+
